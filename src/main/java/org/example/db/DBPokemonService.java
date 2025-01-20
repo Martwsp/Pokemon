17365366aba050c9725a3256cd0417b44109a8a8
MartinVymlatil <vymlatilmartin8@gmail.com>
2025-01-20 13:59:53 +0100

    improved exception hnadling

--- src/main/java/org/example/db/DBPokemonService.java
+++ src/main/java/org/example/db/DBPokemonService.java
@@ -1,12 +1,8 @@
 package org.example.db;
 
-import org.slf4j.Logger;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 public class DBPokemonService {
     private static final String READ_WILD = "SELECT * FROM pokemons WHERE owner_id IS NULL";
@@ -24,140 +20,114 @@ public class DBPokemonService {
                     "GROUP BY t.id,t.name " +
                     "ORDER BY pokemons_owned DESC";
 
-    private static final Logger logger = getLogger(DBPokemonService.class);
 
 
-    public List<Pokemon> readWild() {
-        try (Connection connection = HikariCPDataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(READ_WILD)) {
+    public List<Pokemon> readWild() throws SQLException,ExceptionInInitializerError {
+        Connection connection = HikariCPDataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(READ_WILD);
 
-            ResultSet resultSet = statement.executeQuery();
-            List<Pokemon> pokemons = new ArrayList<>();
-            while (resultSet.next()) {
-                pokemons.add(new Pokemon(
-                        resultSet.getInt("id"),
-                        resultSet.getString("name")
-                ));
-            }
-            return pokemons;
-        } catch (SQLException e) {
-            logger.error("Error while reading wild pokemon!", e);
-            return null;
+        ResultSet resultSet = statement.executeQuery();
+        List<Pokemon> pokemons = new ArrayList<>();
+        while (resultSet.next()) {
+            pokemons.add(new Pokemon(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name")
+            ));
         }
+        return pokemons;
     }
 
-    public int createOwned(String name, int owner_id) {
-        try (Connection connection = HikariCPDataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(CREATE_OWNED, PreparedStatement.RETURN_GENERATED_KEYS)) {
 
-            statement.setString(1, name);
-            statement.setInt(2, owner_id);
+    public int createOwned(String name, int owner_id) throws SQLException,ExceptionInInitializerError {
+        Connection connection = HikariCPDataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(CREATE_OWNED, PreparedStatement.RETURN_GENERATED_KEYS);
 
-            return statement.executeUpdate();
-        } catch (SQLException e) {
-            logger.error("Error while creating pokemon!", e);
-            return 0;
-        }
+        statement.setString(1, name);
+        statement.setInt(2, owner_id);
+
+        return statement.executeUpdate();
     }
 
-    public int createWild(String name) {
-        try (Connection connection = HikariCPDataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(CREATE_WILD, PreparedStatement.RETURN_GENERATED_KEYS)) {
 
-            statement.setString(1, name);
+    public int createWild(String name) throws SQLException,ExceptionInInitializerError {
+        Connection connection = HikariCPDataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(CREATE_WILD, PreparedStatement.RETURN_GENERATED_KEYS);
 
-            return statement.executeUpdate();
-        } catch (SQLException e) {
-            logger.error("Error while creating wild pokemon!", e);
-            return 0;
-        }
+        statement.setString(1, name);
+
+        return statement.executeUpdate();
     }
 
-    public int catchPokemon(Pokemon pokemon, Trainer trainer) {
-        try (Connection connection = HikariCPDataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(CATCH)) {
 
-            statement.setInt(1, trainer.getId());
-            statement.setInt(2, pokemon.getId());
+    public int catchPokemon(Pokemon pokemon, Trainer trainer) throws SQLException,ExceptionInInitializerError {
+        Connection connection = HikariCPDataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(CATCH);
 
-            return statement.executeUpdate();
-        } catch (SQLException e) {
-            logger.error("Error while editing pokemon!", e);
-            return 0;
-        }
+        statement.setInt(1, trainer.getId());
+        statement.setInt(2, pokemon.getId());
+
+        return statement.executeUpdate();
     }
 
-    public List<Trainer> readTrainers() {
-        try (Connection connection = HikariCPDataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(READ_TRAINERS)) {
 
-            ResultSet resultSet = statement.executeQuery();
-            List<Trainer> trainers = new ArrayList<>();
-            while (resultSet.next()) {
-                trainers.add(new Trainer(
-                        resultSet.getInt("id"),
-                        resultSet.getString("name")
-                ));
-            }
-            return trainers;
-        } catch (SQLException e) {
-            logger.error("Error while reading trainers!", e);
-            return null;
+    public List<Trainer> readTrainers() throws SQLException,ExceptionInInitializerError {
+        Connection connection = HikariCPDataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(READ_TRAINERS);
+
+        ResultSet resultSet = statement.executeQuery();
+        List<Trainer> trainers = new ArrayList<>();
+        while (resultSet.next()) {
+            trainers.add(new Trainer(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name")
+            ));
         }
+        return trainers;
     }
 
-    public int createTrainer(String name) {
-        try (Connection connection = HikariCPDataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(CREATE_TRAINERS, PreparedStatement.RETURN_GENERATED_KEYS)) {
 
-            statement.setString(1, name);
+    public int createTrainer(String name) throws SQLException,ExceptionInInitializerError {
+        Connection connection = HikariCPDataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(CREATE_TRAINERS, PreparedStatement.RETURN_GENERATED_KEYS);
 
-            return statement.executeUpdate();
-        } catch (SQLException e) {
-            logger.error("Error while creating wild pokemon!", e);
-            return 0;
-        }
+        statement.setString(1, name);
+
+        return statement.executeUpdate();
     }
 
-    public List<Pokemon> readOwnedBy(Trainer trainer) {
-        try (Connection connection = HikariCPDataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(READ_OWNED_BY)) {
 
-            statement.setInt(1, trainer.getId());
+    public List<Pokemon> readOwnedBy(Trainer trainer) throws SQLException,ExceptionInInitializerError {
+        Connection connection = HikariCPDataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(READ_OWNED_BY);
 
-            ResultSet resultSet = statement.executeQuery();
-            List<Pokemon> pokemons = new ArrayList<>();
-            while (resultSet.next()) {
-                pokemons.add(new Pokemon(
-                        resultSet.getInt("id"),
-                        resultSet.getString("name")
-                ));
-            }
-            return pokemons;
-        } catch (SQLException e) {
-            logger.error("Error while reading owned pokemon!", e);
-            return null;
+        statement.setInt(1, trainer.getId());
+
+        ResultSet resultSet = statement.executeQuery();
+        List<Pokemon> pokemons = new ArrayList<>();
+        while (resultSet.next()) {
+            pokemons.add(new Pokemon(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name")
+            ));
         }
+        return pokemons;
     }
 
-    public List<Trainer> readBest() {
-        try (Connection connection = HikariCPDataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(READ_BEST)) {
 
-            ResultSet resultSet = statement.executeQuery();
-            List<Trainer> trainers = new ArrayList<>();
-            while (resultSet.next()) {
-                trainers.add(new Trainer(
-                        resultSet.getInt("id"),
-                        resultSet.getString("name"),
-                        resultSet.getInt("pokemons_owned")
-                ));
-            }
-            return trainers;
-        } catch (SQLException e) {
-            logger.error("Error while reading trainers!", e);
-            return null;
+    public List<Trainer> readBest() throws SQLException,ExceptionInInitializerError {
+        Connection connection = HikariCPDataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(READ_BEST);
+
+        ResultSet resultSet = statement.executeQuery();
+        List<Trainer> trainers = new ArrayList<>();
+        while (resultSet.next()) {
+            trainers.add(new Trainer(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("pokemons_owned")
+            ));
         }
+        return trainers;
     }
 
 }
--- src/main/java/org/example/service/CRUDManager.java
+++ src/main/java/org/example/service/CRUDManager.java
@@ -4,10 +4,16 @@ import org.example.db.DBPokemonService;
 import org.example.db.Pokemon;
 import org.example.db.Trainer;
 import org.example.utility.InputUtils;
+import org.slf4j.Logger;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class CRUDManager {
+
+    private static final Logger logger = getLogger(CRUDManager.class);
 
     private final DBPokemonService pokemonService;
 
@@ -30,40 +36,55 @@ public class CRUDManager {
             System.out.println("_______________________________________________________________");
 
             final int choice = InputUtils.readInt();
-            switch (choice) {
-                case 0 -> printTrainers();
-                case 1 -> printWild();
-                case 2 -> catchWild();
-                case 3 -> printTrainerPokemon();
-                case 4 -> printTrainerBest();
-                case 5 -> createTrainer();
-                case 6 -> createPokemonOwned();
-                case 7 -> createWild();
-                case 8 -> {
-                    return;
+            try {
+                switch (choice) {
+                    case 0 -> printTrainers();
+                    case 1 -> printWild();
+                    case 2 -> catchWild();
+                    case 3 -> printTrainerPokemon();
+                    case 4 -> printTrainerBest();
+                    case 5 -> createTrainer();
+                    case 6 -> createPokemonOwned();
+                    case 7 -> createWild();
+                    case 8 -> {
+                        return;
+                    }
+                    default -> System.out.println("Invalid choice");
                 }
-                default -> System.out.println("Invalid choice");
+            } catch (ExceptionInInitializerError e) {
+                logger.error("Error while connecting to database", e);
+                System.out.println("Make sure that a valid database is available at port 3306");
+                System.out.println("and correct username and password are specified in application.properties");
+                return;
             }
         }
     }
 
     private void printTrainers() {
-        System.out.println("Trainers:");
-        System.out.println("_________");
-        final List<Trainer> trainers = pokemonService.readTrainers();
-        trainers.forEach(System.out::println);
-        if (trainers.isEmpty()) System.out.println("No trainers, add a trainer first.");
+        try {
+            System.out.println("Trainers:");
+            System.out.println("_________");
+            final List<Trainer> trainers = pokemonService.readTrainers();
+            trainers.forEach(System.out::println);
+            if (trainers.isEmpty()) System.out.println("No trainers, add a trainer first.");
+        } catch (SQLException e) {
+            logger.error("Error while reading trainers!", e);
+        }
     }
 
     private void printWild() {
-        System.out.println("Wild pokemon:");
-        System.out.println("_____________");
-        final List<Pokemon> pokemon = pokemonService.readWild();
-        pokemon.forEach(System.out::println);
-        if (pokemon.isEmpty()) System.out.println("There are no wild pokemon.");
+        try {
+            System.out.println("Wild pokemon:");
+            System.out.println("_____________");
+            final List<Pokemon> pokemon = pokemonService.readWild();
+            pokemon.forEach(System.out::println);
+            if (pokemon.isEmpty()) System.out.println("There are no wild pokemon.");
+        } catch (SQLException e) {
+            logger.error("Error while reading wild pokemon!", e);
+        }
     }
 
-    private Trainer pickTrainer() {
+    private Trainer pickTrainer() throws SQLException {
         Trainer trainer;
         while (true) {
             System.out.println("0. Cancel");
@@ -91,100 +112,133 @@ public class CRUDManager {
     }
 
     private void catchWild() {
-        Pokemon pokemon;
+        try {
+            Pokemon pokemon;
 
-        final List<Pokemon> pokemons = pokemonService.readWild();
+            final List<Pokemon> pokemons = pokemonService.readWild();
 
-        if (pokemons.isEmpty()) {
-            System.out.println("There are no wild pokemon to catch!");
-            return;
-        }
-
-        System.out.println("Which trainer is catching?");
-        System.out.println("__________________________");
-        Trainer trainer = pickTrainer();
-        if (trainer == null) return;
-
-        while (true) {
-            System.out.println("Which pokemon is being caught by " + trainer + "?");
-            System.out.println("________________________________________________________");
-            System.out.println("0. Cancel");
-
-            for (int i = 0; i < pokemons.size(); i++) {
-                System.out.println((i + 1) + ". " + pokemons.get(i));
-            }
-
-            int choicePokemon = InputUtils.readInt();
-
-            if (choicePokemon == 0) {
+            if (pokemons.isEmpty()) {
+                System.out.println("There are no wild pokemon to catch!");
                 return;
-            } else if (choicePokemon < 1 || choicePokemon > pokemons.size()) {
-                System.out.println("Invalid choice");
-            } else {
-                pokemon = pokemons.get(choicePokemon - 1);
-                break;
             }
-        }
-        if (pokemonService.catchPokemon(pokemon, trainer) > 0) {
-            System.out.println(trainer + " caught the " + pokemon);
-        } else {
-            System.out.println("Pokemon could not be caught.");
+
+            System.out.println("Which trainer is catching?");
+            System.out.println("__________________________");
+            Trainer trainer = pickTrainer();
+            if (trainer == null) {
+                System.out.println("Cancelled");
+                return;
+            }
+
+            while (true) {
+                System.out.println("Which pokemon is being caught by " + trainer + "?");
+                System.out.println("________________________________________________________");
+                System.out.println("0. Cancel");
+
+                for (int i = 0; i < pokemons.size(); i++) {
+                    System.out.println((i + 1) + ". " + pokemons.get(i));
+                }
+
+                int choicePokemon = InputUtils.readInt();
+
+                if (choicePokemon == 0) {
+                    return;
+                } else if (choicePokemon < 1 || choicePokemon > pokemons.size()) {
+                    System.out.println("Invalid choice");
+                } else {
+                    pokemon = pokemons.get(choicePokemon - 1);
+                    break;
+                }
+            }
+            if (pokemonService.catchPokemon(pokemon, trainer) > 0) {
+                System.out.println(trainer + " caught the " + pokemon);
+            } else {
+                System.out.println("Pokemon could not be caught.");
+            }
+        } catch (SQLException e) {
+            logger.error("Error while catching wild pokemon!", e);
         }
     }
 
     private void printTrainerPokemon() {
-        System.out.println("Show pokemon of which trainer?");
-        Trainer trainer = pickTrainer();
-        if (trainer == null) return;
+        try {
+            System.out.println("Show pokemon of which trainer?");
+            Trainer trainer = pickTrainer();
+            if (trainer == null) {
+                System.out.println("Cancelled");
+                return;
+            }
 
-        pokemonService.readOwnedBy(trainer).forEach(System.out::println);
+            pokemonService.readOwnedBy(trainer).forEach(System.out::println);
+        } catch (SQLException e) {
+            logger.error("Error while reading this trainer's pokemon!", e);
+        }
     }
 
     private void printTrainerBest() {
-        final List<Trainer> trainers = pokemonService.readBest();
-        System.out.println("Trainers by pokemon:");
-        System.out.println("____________________");
-        for (int i = 0; i < trainers.size(); i++) {
-            System.out.println((i + 1) + ": " + trainers.get(i).getName() + " with " + trainers.get(i).getPokemonOwned() + " pokemon owned.");
+        try {
+            final List<Trainer> trainers = pokemonService.readBest();
+            System.out.println("Trainers by pokemon:");
+            System.out.println("____________________");
+            for (int i = 0; i < trainers.size(); i++) {
+                System.out.println((i + 1) + ": " + trainers.get(i).getName() + " with " + trainers.get(i).getPokemonOwned() + " pokemon owned.");
+            }
+        } catch (SQLException e) {
+            logger.error("Error while reading trainers ordered by pokemon owned!", e);
         }
     }
 
     private void createTrainer() {
-        System.out.println("Enter name:");
-        final String name = InputUtils.readString();
-        final int result = pokemonService.createTrainer(name);
-        if (result > 0) {
-            System.out.println(name + " successfully added");
-        } else {
-            System.out.println("Error, could not add trainer.");
+        try {
+            System.out.println("Enter name:");
+            final String name = InputUtils.readString();
+            final int result = pokemonService.createTrainer(name);
+            if (result > 0) {
+                System.out.println(name + " successfully added");
+            } else {
+                System.out.println("Error, could not add trainer.");
+            }
+        } catch (SQLException e) {
+            logger.error("Error while creating trainer!", e);
         }
     }
 
     private void createPokemonOwned() {
-        int owner_id;
-        System.out.println("Which trainer is getting a new pokemon?");
-        Trainer trainer = pickTrainer();
-        if (trainer == null) return;
-        owner_id = trainer.getId();
+        try {
+            int owner_id;
+            System.out.println("Which trainer is getting a new pokemon?");
+            Trainer trainer = pickTrainer();
+            if (trainer == null) {
+                System.out.println("Cancelled");
+                return;
+            }
+            owner_id = trainer.getId();
 
-        System.out.println("What is the pokemon's name?");
-        final String name = InputUtils.readString();
-        final int result = pokemonService.createOwned(name, owner_id);
-        if (result > 0) {
-            System.out.println(name + " is now " + trainer + "'s!");
-        } else {
-            System.out.println("Error, could not create pokemon.");
+            System.out.println("What is the pokemon's name?");
+            final String name = InputUtils.readString();
+            final int result = pokemonService.createOwned(name, owner_id);
+            if (result > 0) {
+                System.out.println(name + " is now " + trainer + "'s!");
+            } else {
+                System.out.println("Error, could not create pokemon.");
+            }
+        } catch (SQLException e) {
+            logger.error("Error while creating pokemon!", e);
         }
     }
 
     private void createWild() {
-        System.out.println("What is the pokemon's name?");
-        final String name = InputUtils.readString();
-        final int result = pokemonService.createWild(name);
-        if (result > 0) {
-            System.out.println("A wild " + name + " appears!");
-        } else {
-            System.out.println("Error, could not create pokemon.");
+        try {
+            System.out.println("What is the pokemon's name?");
+            final String name = InputUtils.readString();
+            final int result = pokemonService.createWild(name);
+            if (result > 0) {
+                System.out.println("A wild " + name + " appears!");
+            } else {
+                System.out.println("Error, could not create pokemon.");
+            }
+        } catch (SQLException e) {
+            logger.error("Error while creating wild pokemon!", e);
         }
     }
 }
